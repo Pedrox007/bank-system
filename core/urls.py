5287a255df9a640392772c63c7518d946212c792
Ana Carolina da Silva Patricio <apatricio@cpqd.com.br>
2023-05-14 18:22:48 -0300

    Adding url

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from core.views import create_account
+from core.views import create_account, check_balance
 
 urlpatterns = [
     path('create-account/',
          create_account,
          name='create-account'),
+    path('check-balance/',
+        check_balance,
+        name='check-balance'),
 ]
