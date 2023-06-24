1feb832b2abe5faa27897705bf14c868419ab770
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2023-06-24 13:09:36 -0300

    fix: secret key on test_settings

--- banksystem/test_settings.py
+++ banksystem/test_settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'test-secret-key')
 
 DEBUG = True
 
